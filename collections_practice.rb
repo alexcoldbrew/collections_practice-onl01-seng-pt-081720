31dbb6badf804cc1e05fb469bd5e042ab1223a63
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-26 00:02:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,7 +46,7 @@ end
 def add_s(array)
   new_array = []
   array.each_with_index.collect do |word, index|
-    if index[1]
+    if index == 1
       new_array << word
     else
       new_array << word + "s"
